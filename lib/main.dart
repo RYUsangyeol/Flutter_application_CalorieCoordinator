586026002d699a87c24fe8baebb4a0901a910c57
RYUsangyeol <rsy3571@gmail.com>
2024-01-11 21:23:52 +0900

    [first commit]

--- lib/main.dart
+++ lib/main.dart
@@ -73,7 +73,7 @@ class _MyHomePageState extends State<MyHomePage>
   }
 
   @override
-  void dispose() {
+  void dispose() {git push origin main
     // 메모리 누수 방지
     controller!.dispose();
     super.dispose();
